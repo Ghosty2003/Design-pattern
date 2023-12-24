cfca8c93fabb0f38f58c4f2e8d200405185bd6fb
RayyyyyRGB <134457384+RayyyyyRGB@users.noreply.github.com>
2023-12-24 13:56:33 +0800

    代理模式

---
+++ Proxy/Proxy.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <iomanip>
+#include <cstdio>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+// 花束接口
+class IBouquet {
+public:
+    virtual void deliver(std::string address) = 0;
+};
+
+// 花店类
+class FlowerShop : public IBouquet {
+public:
+    void deliver(std::string address) override;
+};
+
+// 送货代理类
+class DeliveryProxy : public IBouquet {
+private:
+    FlowerShop* flowerShop;
+public:
+    DeliveryProxy();
+    void deliver(std::string address) override;
+};
+
+// 抽象工厂模式
+
+// 前置声明
+class HighEndBouquet;
+class EconomyBouquet;
+
+// 花束工厂接口
+class IBouquetFactory {
+public:
+    virtual IBouquet* createBouquet() = 0;
+};
+
+// 高端花束类
+class HighEndBouquet : public IBouquet {
+public:
+    void deliver(std::string address) override;
+};
+
+// 经济型花束类
+class EconomyBouquet : public IBouquet {
+public:
+    void deliver(std::string address) override;
+};
+
+// 高端花束工厂
+class HighEndBouquetFactory : public IBouquetFactory {
+public:
+    IBouquet* createBouquet() override;
+private:
+    std::string Hf[2];
+};
+
+// 经济型花束工厂
+class EconomyBouquetFactory : public IBouquetFactory {
+public:
+    IBouquet* createBouquet() override;
+private:
+    std::string Ef[2];
+};
