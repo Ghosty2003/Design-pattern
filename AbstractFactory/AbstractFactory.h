5b9bf3aa01fac533c28037d6fd94a96d5e24dfa2
RayyyyyRGB <134457384+RayyyyyRGB@users.noreply.github.com>
2023-12-27 20:11:47 +0800

    Update AbstractFactory.h
    区分类名

--- AbstractFactory/AbstractFactory.h
+++ AbstractFactory/AbstractFactory.h
@@ -11,103 +11,105 @@
 using namespace std;
 
 // 花束接口
-class IBouquet {
+class IBouquetRQX {
 public:
     virtual void deliver(std::string address) = 0;
 };
 
 // 花店类
-class Flowershop : public IBouquet {
+class FlowershopRQX : public IBouquetRQX {
 public:
     void deliver(std::string address) override;
 };
 
 // 送货代理类
-class DeliveryProxy : public IBouquet {
+class DeliveryProxyRQX : public IBouquetRQX {
 private:
-    Flowershop* flowerShop;
+    FlowershopRQX* flowerShop;
 public:
-    DeliveryProxy();
+    DeliveryProxyRQX();
     void deliver(std::string address) override;
 };
 
 // 抽象工厂模式
 
 // 前置声明
-class HighEndBouquet;
-class EconomyBouquet;
+class HighEndBouquetRQX;
+class EconomyBouquetRQX;
 
 // 花束工厂接口
-class IBouquetFactory {
+class IBouquetFactoryRQX {
 public:
-    virtual IBouquet* createBouquet() = 0;
+    virtual IBouquetRQX* createBouquet() = 0;
 };
 
 // 高端花束类
-class HighEndBouquet : public IBouquet {
+class HighEndBouquetRQX : public IBouquetRQX {
 public:
     void deliver(std::string address) override;
 };
 
 // 经济型花束类
-class EconomyBouquet : public IBouquet {
+class EconomyBouquetRQX : public IBouquetRQX {
 public:
     void deliver(std::string address) override;
 };
 
 // 高端花束工厂
-class HighEndBouquetFactory : public IBouquetFactory {
+class HighEndBouquetFactoryRQX : public IBouquetFactoryRQX {
 public:
-    IBouquet* createBouquet() override;
+    IBouquetRQX* createBouquet() override;
 private:
     std::string Hf[2];
 };
 
 // 经济型花束工厂
-class EconomyBouquetFactory : public IBouquetFactory {
+class EconomyBouquetFactoryRQX : public IBouquetFactoryRQX {
 public:
-    IBouquet* createBouquet() override;
+    IBouquetRQX* createBouquet() override;
 private:
     std::string Ef[2];
 };
 
-void Flowershop::deliver(std::string address) {
-    cout << "花店将花束送到地址：" << address << endl;
+
+
+void FlowershopRQX::deliver(std::string address) {
+    cout << "The flower shop will deliver the bouquet to the address:" << address << endl;
 }
 
-DeliveryProxy::DeliveryProxy() {
-    flowerShop = new Flowershop();
+DeliveryProxyRQX::DeliveryProxyRQX() {
+    flowerShop = new FlowershopRQX();
 }
 
-void DeliveryProxy::deliver(std::string address) {
-    cout << "代理收到送货请求，将协调外部快递公司来完成送货任务。" << endl;
+void DeliveryProxyRQX::deliver(std::string address) {
+    cout << "The agent receives the delivery request and will coordinate with an external courier company to complete the delivery task." << endl;
     flowerShop->deliver(address);
 }
 
-void HighEndBouquet::deliver(std::string address) {
-    cout << "高端花束将花束送到地址：" << address << endl;
+void HighEndBouquetRQX::deliver(std::string address) {
+    cout << "High-end bouquet will be delivered to the address:" << address << endl;
 }
 
-void EconomyBouquet::deliver(std::string address) {
-    cout << "经济型花束将花束送到地址：" << address << endl;
+void EconomyBouquetRQX::deliver(std::string address) {
+    cout << "Economical bouquet will be delivered to the address: " << address << endl;
 }
 
-IBouquet* HighEndBouquetFactory::createBouquet() {
-    cout << "创建高端花束。" << endl;
+IBouquetRQX* HighEndBouquetFactoryRQX::createBouquet() {
+    cout << "Create a high-end bouquet.*********" << endl;
     // 实际创建高端花束的代码
-    Hf[0] = "弗洛伊德玫瑰";
-    Hf[1] = "岚黛";
+    Hf[0] = "Freudian rose";
+    Hf[1] = "Lan Dai";
     std::cout << Hf[0] << ' ' << Hf[1] << std::endl;
-    return new HighEndBouquet();
+    return new HighEndBouquetRQX();
 }
 
-IBouquet* EconomyBouquetFactory::createBouquet() {
-    cout << "创建经济型花束。" << endl;
+IBouquetRQX* EconomyBouquetFactoryRQX::createBouquet() {
+    cout << "Create an economical bouquet.*********" << endl;
     // 实际创建经济型花束的代码
-    Ef[0] = "波斯菊";
-    Ef[1] = "向日葵";
+    Ef[0] = "coreopsis";
+    Ef[1] = "sunflower";
     std::cout << Ef[0] << ' ' << Ef[1] << std::endl;
-    return new EconomyBouquet();
+    return new EconomyBouquetRQX();
 }
 
 void testAbstractFactory();
