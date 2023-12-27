ed1fc07714ec05825778ed2cb0ff01bd497eb07d
RayyyyyRGB <134457384+RayyyyyRGB@users.noreply.github.com>
2023-12-27 20:14:18 +0800

    Update Proxy.cpp
    英文输出

--- Proxy/Proxy.cpp
+++ Proxy/Proxy.cpp
@@ -9,19 +9,21 @@
 
 using namespace std;
 
+
+
 void testProxy() {
     // 代理模式示例
     IBouquet* deliveryProxy = new DeliveryProxy();
-    deliveryProxy->deliver("曼彻斯特-维多利亚街");
+    deliveryProxy->deliver("*Manchester Victoria Street*");
 
     // 抽象工厂模式示例
     IBouquetFactory* highEndFactory = new HighEndBouquetFactory();
     IBouquet* highEndBouquet = highEndFactory->createBouquet();
-    highEndBouquet->deliver("曼彻斯特-维多利亚街-曼彻斯特大教堂");
+    highEndBouquet->deliver("*Manchester - Victoria Street - Manchester Cathedral*");
 
     IBouquetFactory* economyFactory = new EconomyBouquetFactory();
     IBouquet* economyBouquet = economyFactory->createBouquet();
-    economyBouquet->deliver("曼彻斯特-维多利亚街-曼彻斯特大学");
+    economyBouquet->deliver("*Manchester - Victoria Street - Manchester University*");
 
     delete deliveryProxy;
     delete highEndFactory;
